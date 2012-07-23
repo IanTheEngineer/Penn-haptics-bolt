ab673de6ec7ff549ac91bb3c47273aff6b1b4dfd
Vivian Chu <chuv@seas.upenn.edu>
2012-07-23 11:10:16 -0400

    Small changes to matlab files - fixed bug and added support for loading all mat files

--- matlab/haptics/biotacs/utilities/load_all_data.m
+++ matlab/haptics/biotacs/utilities/load_all_data.m
@@ -1,4 +1,4 @@
-function [loaded_data, total_files] = load_all_data(path)
+function [loaded_data, total_files] = load_all_data(path, mat)
 % This function is similar to load_data_from_file but instead of a single
 % file, it will load a directory of files, including other directories.  It
 % expects the data to be stored in a heirachal fashion.  You must pass in
@@ -36,29 +36,39 @@ end
 data_file_names = dirr(path);
 data_files = struct();
 total_files = 1;
-for i = 1:numel(data_file_names)
-    data_files(i).name = data_file_names(i).name;
-    data = struct();
-    for j = 1:numel(data_file_names(i).isdir)
-        fileID = fopen(strcat(path,dirLink,data_file_names(i).name,dirLink,data_file_names(i).isdir(j).name));
-        
-        % Populate information about the file
-        name = regexp(data_file_names(i).isdir(j).name,'[0-9]','split');
-        data(j).motion = name(1);
-        data(j).name = data_file_names(i).name;
-        data(j).category = i;
-        
-        % Load the data depending on .json or .txt format
-        if (sum(strfind(data_file_names(i).isdir(j).name, '.json')) > 0)
-            [all_data, number_fingers] = process_json_file(fileID);
-        else
-            [all_data, number_fingers] = process_txt_file(fileID);
-        end
 
-        data(j).values = all_data;
-        total_files = total_files + 1;
+if (mat)
+    for i = 1:numel(data_file_names)
+        data = load_data_from_path(strcat(path, dirLink,data_file_names(i).name));
+        total_files = total_files +1;
+        data_files(i).values = data;
+        data_files(i).name = data_file_names(i).name;
     end
-    data_files(i).data = data;
+else
+    for i = 1:numel(data_file_names)
+        data_files(i).name = data_file_names(i).name;
+        data = struct();
+        for j = 1:numel(data_file_names(i).isdir)
+            fileID = fopen(strcat(path,dirLink,data_file_names(i).name,dirLink,data_file_names(i).isdir(j).name));
+
+            % Populate information about the file
+            name = regexp(data_file_names(i).isdir(j).name,'[0-9]','split');
+            data(j).motion = name(1);
+            data(j).name = data_file_names(i).name;
+            data(j).category = i;
+
+            % Load the data depending on .json or .txt format
+            if (sum(strfind(data_file_names(i).isdir(j).name, '.json')) > 0)
+                [all_data, number_fingers] = process_json_file(fileID);
+            else
+                [all_data, number_fingers] = process_txt_file(fileID);
+            end
+
+            data(j).values = all_data;
+            total_files = total_files + 1;
+        end
+        data_files(i).data = data;
+    end
 end
 
 loaded_data = data_files;
--- matlab/haptics/biotacs/utilities/process_mat_file.m
+++ matlab/haptics/biotacs/utilities/process_mat_file.m
@@ -4,10 +4,17 @@ function [ all_data, number_fingers ] = process_mat_file( fileID )
 % Load file
 load(fileID);
 
+% Delete fileID Name
+clear fileID;
+
 % Grab variable
 variables = who('');
 
-all_data = eval(cell2mat(variables(1)));
+if (isequal(variables{1}, 'variables'))
+    all_data = eval(cell2mat(variables(2)));
+else
+    all_data = eval(cell2mat(variables(1)));
+end
 
 if (isfield(all_data, 'biotac'))
     number_fingers = size(all_data.biotac,2);
