8c47aab15ba56b6c0b0d5618aa9f3a40ac8ccfcb
Vivian Chu <chuv@seas.upenn.edu>
2012-08-13 18:56:18 -0400

    New stack for whole pipe and new package for utilities

---
+++ ros/haptics/bolt_haptic_learning/bolt_learning_utilities/src/bolt_object.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest("bolt_learning_utilities")
+import rospy
+import numpy as np 
+
+# Class to store an entire run 
+class BoltPR2Motion(object):
+
+    def __init__(self):
+
+        # 2 finger x 19 x n
+        self.electrodes = []
+        # 2 finger x n
+        self.tdc = []
+        self.tac = []
+        self.pdc = []
+        self.pac = []
+       
+        # Gripper
+        # 1 x n
+        self.gripper_velocity = []
+        self.gripper_position = []
+        self.gripper_effort = []
+
+        # Accelerometer
+        # 3 x n
+        self.accelerometer = [] 
+
+        # Store state
+        self.state = 0
+   
+        # Maybe store raw electrodes
