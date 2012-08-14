7470824d3e3b8fd75880a27dc32913ab8a98d881
Ian McMahon <imcmahon01@gmail.com>
2012-08-13 23:46:44 -0400

    Night backup on test_master_thread.py

--- ros/haptics/bolt_haptic_learning/language_test_pipe/src/test_master_thread.py
+++ ros/haptics/bolt_haptic_learning/language_test_pipe/src/test_master_thread.py
@@ -17,43 +17,38 @@ from std_msgs.msg import Int8
 
 import multiprocessing
 
-class MyFancyClass(object):
-    
-    def __init__(self, name):
-        self.name = name
-    
-    def do_something(self):
-        proc_name = multiprocessing.current_process().name
-        current_motion = BoltPR2MotionObj()
-        current_motion.tdc = proc_name
-        print current_motion
-        #print 'Doing something fancy in %s for %s!' % (proc_name, self.name)
 
-def worker(q):
-    obj = q.get()
-    obj.do_something()
+def processMotion(task_queue, result_queue):
+    #Grab the current motion from the queue
+    current_motion = task_queue.get()
+
+    #operate to determine stuff
+    answer = current_motion.tdc
+
+    result_queue.put( answer )
+    
+
 
 class LanguageTestMainThread:
 
-  def __init__(self):
-    rospy.init_node('Language Test Main Thread')
-    rospy.loginfo('main language test thread initializing...')
+    def __init__(self):
+        rospy.init_node('Language Test Main Thread')
+        rospy.loginfo('main language test thread initializing...')
 
-  def start_listeners(self):
-    pass
-    #Start BioTac Subscriber
-    #rospy.Subscriber('biotac_pub', BioTacHand, self.biotacCallback,queue_size=1000)
-    #Start Accelerometer Subscriber
-    #rospy.Subscriber('/pr2_gripper_accelerometer/data', PR2GripperAccelerometerData, self.accelerometerCallback,queue_size=1000)
-    #Start Gripper Controller State Subscriber
-    #rospy.Subscriber('/simple_gripper_controller_state', Int8, self.gripperControllerCallback, queue_size=1000)
+    def start_listeners(self):
+        #Start BioTac Subscriber
+        rospy.Subscriber('biotac_pub', BioTacHand, self.biotacCallback,queue_size=1000)
+        #Start Accelerometer Subscriber
+        rospy.Subscriber('/pr2_gripper_accelerometer/data', PR2GripperAccelerometerData, self.accelerometerCallback,queue_size=1000)
+        #Start Gripper Controller State Subscriber
+        rospy.Subscriber('/simple_gripper_controller_state', Int8, self.gripperControllerCallback, queue_size=1000)
 
 
-  def accelerometerCallback(self, accelerometer_data):
-    pass
+    def accelerometerCallback(self, accelerometer_data):
+        pass
 
-  def biotacCallback(self, biotac_data):
-    pass
+    def biotacCallback(self, biotac_data):
+        pass
     # Stores the frame count into the message
     #data.header.frame_id = self.frame_count
     #Add Data to some buffer
@@ -61,25 +56,37 @@ class LanguageTestMainThread:
     #self.frame_count += 1
 
 
-  def gripperControllerCallback(self, gripper_status_data):
-    pass
+    def gripperControllerCallback(self, gripper_status_data):
+        pass
 
 
 def main(argv):
 
     #Look for Change in status
     #If change detected, close data (file?) and pass it into a new thread
-    queue = multiprocessing.Queue()
+
+    # Establish communication queues
+    tasks = multiprocessing.Queue()
+    results = multiprocessing.Queue()
+    num_tasks = 0
     for i in range(1,1000):
       if i%100 is 0 and i < 500:
-        p = multiprocessing.Process(target=worker, args=(queue,))
-        p.start()
-        queue.put(MyFancyClass('Master Thread'))
-    queue.close()
-    queue.join_thread()
-    p.join()
-        
+        new_process = multiprocessing.Process(target=processMotion, args=(tasks,results))
+        recorded_motion = BoltPR2MotionObj()
+        recorded_motion.tdc = i
+        recorded_motion.tac = i
+        recorded_motion.pdc = i
+        recorded_motion.pac = i
+        tasks.put(recorded_motion)
+        new_process.start()
+        num_tasks = num_tasks + 1
 
+    tasks.close()
+    tasks.join_thread()
+    
+    for i in range(num_tasks):
+        result = results.get()
+        print 'Result:', result
 
 if __name__ == '__main__':
   main(sys.argv[1:])
