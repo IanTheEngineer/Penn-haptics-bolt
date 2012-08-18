675fdbda8495ef780d8b69f415dad9403f73b385
Vivian Chu <chuv@seas.upenn.edu>
2012-08-18 15:02:50 -0400

    Added detailed state to training pipe, renamed training pipe to hadjective_training_pipe, moved normalized from extract_feature to utilities

--- ros/haptics/bolt_haptic_learning/bolt_learning_utilities/src/bolt_learning_utilities.py
+++ ros/haptics/bolt_haptic_learning/bolt_learning_utilities/src/bolt_learning_utilities.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest("bolt_learning_utilities")
 import rospy
+import numpy as np
 import cPickle
 import convert_h5_dataset_to_bolt_obj as converth5
 import process_adjective_labels
-
-# Copy over normalize from extract features
 
 # Convert h5 files to BolPR2MotionObj data
 def convertH5ToBoltObjFile(input_file, output_file, save_to_file):
@@ -63,6 +62,7 @@ def loadBoltObjFile(input_file):
     return loaded_data
 
 
+
 # Insert adjective labels 
 def insertAdjectiveLabels(input_boltPR2Motion, output_file, adjective_file, save_to_file):
     """
@@ -85,6 +85,65 @@ def insertAdjectiveLabels(input_boltPR2Motion, output_file, adjective_file, save
 
     return data_with_adjectives
 
+
+
+# Normalizes the given bolt_obj.  Works directly on the object
+def normalize_data(bolt_obj, discard_raw_flag = True):
+    """ 
+    Given a BOLTPR2MotionObj 
+    Normalize the data
+        - Takes the mean and subtract 
+        - Adds a pac_flat field
+
+    Usage: normalize(bolt_obj, discard_flag)
+        
+        Discard flag is optional - default is True
+        Discard flag set False will keep the original raw
+        data, which makes the data twice as large
+    """
+    
+    num_fingers = len(bolt_obj.electrodes)
+
+    # For each finger normalize and store
+    for finger in xrange(num_fingers):
+        # Electrodes
+        electrodes = bolt_obj.electrodes[finger]
+        electrodes_mean = bolt_obj.electrodes_mean[finger]
+        bolt_obj.electrodes_normalized.append(-(electrodes - np.mean(electrodes_mean, axis = 0)))
+
+        # PDC
+        pdc = bolt_obj.pdc[finger]
+        pdc_mean = bolt_obj.pdc_mean[finger]
+        bolt_obj.pdc_normalized.append(pdc - np.mean(pdc_mean))
+
+        # TDC
+        tdc = bolt_obj.tdc[finger]
+        tdc_mean = bolt_obj.tdc_mean[finger]
+        bolt_obj.tdc_normalized.append(tdc - np.mean(tdc_mean))
+
+        # TAC
+        tac = bolt_obj.tac[finger]
+        tac_mean = bolt_obj.tac_mean[finger]
+        bolt_obj.tac_normalized.append(-(tac - np.mean(tac_mean)))
+
+        # PAC
+        pac = bolt_obj.pac[finger]
+        pac_mean = bolt_obj.pac_mean[finger]
+        pac_norm = -(pac - np.mean(pac_mean, axis = 0)) 
+        bolt_obj.pac_normalized.append(pac_norm)
+
+        # Flatten PAC
+        bolt_obj.pac_flat.append( pac.reshape(1,len(pac)*22)[0])
+        bolt_obj.pac_flat_normalized.append( pac_norm.reshape(1, len(pac_norm)*22)[0])
+
+    if discard_raw_flag:
+        # Clear out raw values - comment out later if they want to be stored
+        # Will double the amount of data stored
+        del bolt_obj.pdc[:]
+        del bolt_obj.electrodes[:]
+        del bolt_obj.pac[:]
+        del bolt_obj.tdc[:]
+        del bolt_obj.tac[:]
 
     
         
