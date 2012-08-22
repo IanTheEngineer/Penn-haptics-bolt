396dbc2735bdd86660972000206f27da231ff446
Ian McMahon <imcmahon01@gmail.com>
2012-08-22 17:22:29 -0400

    Added bolt_feature_obj.py for exacting features

---
+++ ros/haptics/bolt_haptic_learning/bolt_learning_utilities/src/bolt_feature_obj.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest("bolt_learning_utilities")
+import rospy
+from numpy import * 
+
+# Class to store an entire run 
+class BoltFeatureObj():
+
+    DISABLED = 0 
+    THERMAL_HOLD = 1 
+    SLIDE = 2 
+    SQUEEZE = 3 
+    TAP = 5 
+    DONE = 4 
+    SLIDE_FAST = 6 
+    CENTER_GRIPPER = 7 
+
+    RIGHT = 0 
+    LEFT = 1 
+
+    def __init__(self):
+
+        # Store features by channel?
+        self.pdc_rise_count = []
+        self.pdc_max = []  
+        self.pdc_area = []
+
+        # Store information about object
+        self.state = self.DISABLED
+        self.detailed_state = []
+
+        # Store information about run motion came from
+        self.name = ""
+        self.run_number = 0
+        self.object_id = 0
+
+        # Store labels
+        self.labels = None
+
