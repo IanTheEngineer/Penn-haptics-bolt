ffb4245d486dad1988da5c5f7dfaf3197411afba
lorenzoriano <lorenzo.riano@berkeley.edu>
2012-10-19 11:25:26 -0700

    added PDC features

--- ros/haptics/bolt_haptic_learning/hadjective_hmm_classifier/src/hadjective_hmm_classifier/training_segments/upenn_features.py
+++ ros/haptics/bolt_haptic_learning/hadjective_hmm_classifier/src/hadjective_hmm_classifier/training_segments/upenn_features.py
@@ -6,6 +6,61 @@ from scipy.signal import filtfilt
 
 from sklearn.decomposition import PCA
 
+def smooth(x,window_len=11,window='hanning'):
+    """smooth the data using a window with requested size.
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    
+    input:
+        x: the input signal 
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+        
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+    
+    see also: 
+    
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+ 
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+    if window_len<3:
+        return x
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    return y
+
 def pdc_features(pdc_data):
     """Area under the curve and max"""
 
@@ -14,8 +69,14 @@ def pdc_features(pdc_data):
     
     area = np.trapz(pdc_data, axis = 0)
     pdc_max = np.max(pdc_data, axis = 0)
-
-    return np.hstack((area, pdc_max))
+    
+    pdc_rise_count = []
+    for finger in (0,1):
+        filtered_pdc = smooth(pdc_data[:,finger],
+                              window_len=50)
+        pdc_rise_count.append(np.max(np.diff(filtered_pdc)))        
+        
+    return np.hstack((area, pdc_max, pdc_rise_count))
 
 def pac_features( pac):
     """
@@ -57,10 +118,12 @@ def pac_features( pac):
                        -0.002780750937971, -0.002297899162801, -0.000008077427051, -0.001331737017349,-0.001575256697592,
                        -0.000046996391356,-0.000734453028953])
         filt_texture = lfilter(b,1,texture)
-        total = 0
-        for elem in filt_texture.tolist():
-            total = total + elem
-        new_mean = total / len(filt_texture)
+        #total = 0
+        #for elem in filt_texture.tolist():
+            #total = total + elem
+        #new_mean = total / len(filt_texture)
+        new_mean = np.mean(filt_texture)
+        
         # Remove signal bias after filtering
         filt_texture = filt_texture - new_mean
     
