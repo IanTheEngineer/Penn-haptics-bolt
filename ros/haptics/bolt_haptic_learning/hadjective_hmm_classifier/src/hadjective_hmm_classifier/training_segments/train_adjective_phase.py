9aa27602a1874a7055b58a7dc6500a1fca276044
lorenzoriano <lorenzo.riano@berkeley.edu>
2013-01-19 12:06:57 -0800

    slightly different training

--- ros/haptics/bolt_haptic_learning/hadjective_hmm_classifier/src/hadjective_hmm_classifier/training_segments/train_adjective_phase.py
+++ ros/haptics/bolt_haptic_learning/hadjective_hmm_classifier/src/hadjective_hmm_classifier/training_segments/train_adjective_phase.py
@@ -12,7 +12,9 @@ import numpy as np
 from static_feature_obj import StaticFeatureObj
 import upenn_features
 from collections import defaultdict
+import sklearn
 from sklearn.externals.joblib import Parallel, delayed
+from sklearn.metrics import f1_score
 
 
 def load_adjective_phase(base_directory):
@@ -39,7 +41,7 @@ def load_adjective_phase(base_directory):
 
     return all_features
 
-def train_adjective_phase_classifier(path, adjective, phase, all_features):
+def original_train_adjective_phase_classifier(path, adjective, phase, all_features):
     """
     Example function on how to access all of the features
     stored in adjective_phase_set
@@ -91,6 +93,67 @@ def train_adjective_phase_classifier(path, adjective, phase, all_features):
         print "Saving file: ", path_name
         cPickle.dump(dataset, f, protocol=cPickle.HIGHEST_PROTOCOL)
 
+def alt_train_adjective_phase_classifier(path, adjective, phase, all_features):
+    """
+    Example function on how to access all of the features
+    stored in adjective_phase_set
+    """
+
+    # File name 
+    dataset_file_name = "_".join(("trained", adjective, phase))+".pkl"
+    newpath = os.path.join(path, "trained_adjective_phase")
+    path_name = os.path.join(newpath, dataset_file_name)
+    
+    if os.path.exists(path_name):
+        print "File %s already exists, skipping it." % path_name
+        return
+
+    #print "Creating adjective %s and phase %s" % (adjective, phase)
+
+    train_set = all_features[adjective][phase]['train']
+    train_X = train_set['features']
+    train_Y = train_set['labels']
+    object_ids = train_set['object_ids']
+
+    print "Training adjective %s and phase %s" %(adjective, phase)
+    
+    
+    #magic training happening here!!!
+    scaler = sklearn.preprocessing.StandardScaler().fit(train_X)
+    train_X = scaler.transform(train_X)    
+    parameters = {'C': np.linspace(0.001,1e6,100),              
+                  'penalty': ['l2','l1'],
+                  'dual': [False],
+                  'class_weight' : ('auto',),
+                  }
+    clf = sklearn.svm.LinearSVC()
+    grid = sklearn.grid_search.GridSearchCV(clf, parameters,
+                                            n_jobs=1,
+                                            score_func=f1_score)
+    grid.fit(train_X, train_Y)
+    trained_clf = grid.best_estimator_
+    #end of magic training!!!
+
+    dataset = all_features[adjective][phase]
+    dataset['adjective'] = adjective
+    dataset['phase'] = phase
+    dataset['classifier'] = trained_clf
+    dataset['scaler'] = scaler
+   
+    test_set = all_features[adjective][phase]['test']
+    test_X = train_set['features']
+    test_Y = train_set['labels']
+    train_score =  grid.best_score_
+    test_score = f1_score(test_Y, trained_clf.predict(test_X))
+    print "The training score is: %.2f, test score is %.2f" % (train_score, test_score)
+
+    # Save the results in the folder
+    with open(path_name, "w") as f:
+        print "Saving file: ", path_name
+        cPickle.dump(dataset, f, protocol=cPickle.HIGHEST_PROTOCOL)
+
+train_adjective_phase_classifier = alt_train_adjective_phase_classifier
+
 def main():
     if len(sys.argv) == 5:
         path, adjective, phase, n_jobs = sys.argv[1:]
@@ -99,7 +162,7 @@ def main():
                 adjective, phase)
 
         loaded_features = load_adjective_phase(path)
-        p = Parallel(n_jobs=n_jobs,verbose=10)
+        p = Parallel(n_jobs=n_jobs,verbose=0)
         p(delayed(train_adjective_phase_classifier)(path, adjective, phase, loaded_features))
 
     elif len(sys.argv) == 4:
@@ -108,7 +171,7 @@ def main():
         print "Training the adjective %s" % adjective
         loaded_features = load_adjective_phase(path)
  
-        p = Parallel(n_jobs=n_jobs,verbose=10)
+        p = Parallel(n_jobs=n_jobs,verbose=0)
         p(delayed(train_adjective_phase_classifier)(path, adjective, phase, loaded_features) 
             for phase in itertools.product(phases))
  
@@ -118,7 +181,7 @@ def main():
         print "Training the all combinations of adjectives and phases"
         loaded_features = load_adjective_phase(path)
  
-        p = Parallel(n_jobs=n_jobs,verbose=10)
+        p = Parallel(n_jobs=n_jobs,verbose=0)
         p(delayed(train_adjective_phase_classifier)(path, adjective, phase, loaded_features) 
             for adjective, phase in itertools.product(adjectives,
                                                       phases))
@@ -132,3 +195,4 @@ def main():
 if __name__=="__main__":
     main()
     print "done"        
+
