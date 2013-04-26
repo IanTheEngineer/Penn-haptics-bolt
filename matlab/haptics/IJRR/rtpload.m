ba61fab89e9612503a9b5e6e0f66211f7756aa50
Vivian Chu <chuv@seas.upenn.edu>
2013-04-25 21:35:24 -0400

    updated matlab scripts for loading CSV files

--- matlab/haptics/IJRR/rtpload.m
+++ matlab/haptics/IJRR/rtpload.m
@@ -21,6 +21,11 @@ if (fid < 0)
   error('Unable to open file %s', filename);
 end
 line = fgetl(fid);
+num_columns = length(regexp(line,',','split'));
+loaded_data = textscan(fid,'%s');
+loaded_data = regexp(loaded_data{1},',','split');
+flattened_data = [loaded_data{:}];
+shaped_data = reshape(flattened_data,[num_columns size(loaded_data,1)]);
 fclose(fid);
 
 % Make sure the file contains something.
@@ -30,14 +35,21 @@ end
 
 % Load the actual data.
 % raw = load(filename);
-raw = importdata(filename);
+% raw = importdata(filename);
 
 % Restructure the data.
 column = 0;
 while (~isempty(line))
   [token,line] = strtok(line,'%,');
   column = column+1;
-  eval([token ' = raw.textdata(:,' num2str(column) ');']);
+  values = [shaped_data(column,:)];
+  test_double = isstrprop(strrep(values{1},'.',''),'digit');
+  if all(test_double)
+      values = str2double(values);
+  end
+%   eval([token ' = raw(:,' num2str(column) ');']);
+%   eval([token ' = [shaped_data(' num2str(column) ', :)];']);
+    eval([token ' = values;']);
 end
 
 % Move to the correct output variables.  The first column
