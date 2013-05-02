39d0b34d92f00eca6d09d625bd2342e34b5b4499
Ian McMahon <imcmahon01@gmail.com>
2013-05-01 20:17:04 -0700

    hadjective speech

--- ros/haptics/bolt_haptic_learning/hadjective_speech/nodes/start_speech.py
+++ ros/haptics/bolt_haptic_learning/hadjective_speech/nodes/start_speech.py
@@ -1,20 +1,86 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('hadjective_speech')
 import rospy
-from hadjective_speech.msg import AdjProbDict
 from sound_play.msg import SoundRequest
 from sound_play.libsoundplay import SoundClient
+from hadjective_speech.msg import AdjList 
 import os
 
-def callback(msg):
-    pass
+voice = 'voice_en1_mbrola'
 
-def listener():
-    rospy.Subscriber("hadj_speech", AdjProbDict, callback)#subscribes to hadjective_speech topic
-    rospy.spin()
+class HadjSpeechDemo:
+  def __init__(self):
+     rospy.init_node('hadjective_speech_node')
+     self.soundhandle = SoundClient()
+     self.voice = 'voice_en1_mbrola'
+     self.soundhandle.say('',voice)
+     os.system("clear")
+     self.color = bcolors()
+     self.main()
+     
 
-def main(argv):
-    listener()
+  def callback(self, msg):
+      #import pdb; pdb.set_trace()
+      #rospy.loginfo(rospy.get_name() + ": I heard %s" % msg.data)
+      felt_msg = "This ob-ject feels"
+      print_msg = "This object feels"
+      for idx, adj in enumerate(msg.adjectives):
+        if idx == len(msg.adjectives) - 1:
+          felt_msg = felt_msg + " and " + str(adj.adj) + "."
+          print_msg = print_msg + " and " + str(adj.adj) + "."
+        else:
+          felt_msg = felt_msg + " " + str(adj.adj) + ","
+          print_msg = print_msg + " " + str(adj.adj) + ","
+      self.soundhandle.say(felt_msg,self.voice)
+      print self.color.set_green(print_msg)
+
+  def listener(self):
+      rospy.Subscriber("hadjective_speech", AdjList, self.callback)#subscribes to hadjective_speech topic
+      rospy.spin()
+
+  def main(self):
+      rospy.sleep(2)
+      query = 'What is your name?'
+      self.soundhandle.say(query,self.voice)
+      response = raw_input( self.color.set_green(query) + self.color.set_blue("\n>> ") )
+      s = "It's a pleasure to meet you, %s. I am GRASPY, the PR2 roe-bot." % response
+      self.soundhandle.say(s,self.voice)
+      s = "It's a pleasure to meet you, %s. I am GRASPY, the PR2 robot." % response
+      print self.color.set_green(s)
+      rospy.sleep(5)
+
+      iteration = []
+      first_time = ['', ''] 
+      next_time = [', again', 'other']
+      iteration = [first_time, next_time]
+      i=0 
+      while not rospy.is_shutdown():
+        if i > 0:
+          i = 1 
+
+        query = 'Shall we begin%s?' %iteration[i][0]
+        self.soundhandle.say(query,self.voice)
+        response = raw_input( self.color.set_green(query) + self.color.set_blue("\n>> ") )
+
+        if response.lower().find('yes') is not -1 or response.lower().find('sure') is not -1: 
+          s = "Excellent! May I have an%s ob-ject to touch?"%iteration[i][1] # Tell me when you are ready."
+          self.soundhandle.say(s,self.voice)
+          s = "Excellent! May I have an%s object to touch?" %iteration[i][1] #\nTell me when you are ready."
+          print self.color.set_green(s)
+          rospy.sleep(4)
+          query = "Tell me when you are ready."
+          self.soundhandle.say(query,self.voice)
+          response = raw_input(self.color.set_green(query) + self.color.set_blue("\n>> ") )
+          self.listener()
+
+          i = i + 1
+        else:
+          s = "That's fine. I'm not going anywhere. I've got all the time in the world."
+          self.soundhandle.say(s,voice)
+          print self.color.set_green(s)
+          rospy.sleep(10)
+
+ 
 
 
 class bcolors:
@@ -42,8 +108,10 @@ class bcolors:
 #http://en.wikibooks.org/wiki/Python_Programming/Input_and_output
 #raw_input
 if __name__ == '__main__':
-    rospy.init_node('hadjective_speech_node')
-    soundhandle = SoundClient()
+    demo = HadjSpeechDemo();
+    demo.main()
+    ''' rospy.init_node('hadjective_speech_node')
+    #soundhandle = SoundClient()
     voice = 'voice_en1_mbrola'
     soundhandle.say('',voice)
     os.system("clear")
@@ -83,6 +151,8 @@ if __name__ == '__main__':
         query = "Tell me when you are ready."
         soundhandle.say(query,voice)
         response = raw_input(color.set_green(query) + color.set_blue("\n>> ") )
+        listener()
+
         i = i + 1
       else:
         s = "That's fine. I'm not going anywhere. I've got all the time in the world."
@@ -90,5 +160,5 @@ if __name__ == '__main__':
         print color.set_green(s)
         rospy.sleep(10)
 
-        #main(sys.argv[1:])
+        #main(sys.argv[1:])'''
 
